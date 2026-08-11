1e247c474c7a143460b53c3ac2f4393b35b01f7f
sabw14 <56178407+sabw14@users.noreply.github.com>
2026-08-11 01:33:48 +0000

    ejercicio creado

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
-const saludo = "¡Hola desde Codespaces!";
-console.log(saludo);
+function calcularPuntosRacha(limiteDias) {
+    let puntosTotales = 0;
+    let diasEspeciales = 0;
+    for (let dia = 1; dia <= limiteDias; dia++) {
+        dia;
+    }
+    return "";
+}
--- app.ts
+++ app.ts
@@ -1,2 +1,20 @@
-const saludo: string = "¡Hola desde Codespaces!";
-console.log(saludo);
+function calcularPuntosRacha(limiteDias: number): string {
+    let puntosTotales = 0
+    let diasEspeciales = 0
+
+    for(let dia = 1; dia <= limiteDias; dia++ ){
+        if(dia % 2 == 0 && !(dia % 5 == 0)){
+            puntosTotales = puntosTotales + 20
+        }else if(dia % 2 != 0 && !(dia % 5 == 0)){
+            puntosTotales = puntosTotales + 10
+        }else if(dia % 5 == 0){
+            puntosTotales = puntosTotales + 50
+            diasEspeciales = diasEspeciales + 1
+        }
+    }
+    return `Puntos totales: ${puntosTotales}, Dias especiales: ${diasEspeciales}`
+}
+
+console.log(calcularPuntosRacha(5));
+console.log(calcularPuntosRacha(3));
+console.log(calcularPuntosRacha(1000000));
